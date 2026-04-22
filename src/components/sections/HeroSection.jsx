b8de49b6bf6ac7d298bfa640a675b244390cfeba
josueamador19 <amadoroscar789@gmail.com>
2026-04-21 19:19:13 -0600

    new video to hero

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -1,4 +1,4 @@
-import bgVideo from '../../assets/hero-video.mp4'
+import bgVideo from '../../assets/hero-video.mov'
 import pumaImg from '../../assets/logo_civil.png'
 
 const BG_VIDEO_SRC = bgVideo
@@ -18,14 +18,14 @@ export default function HeroSection() {
     <div style={{ minHeight: '10vh', maxHeight:'90vh', background: '#0a1628', position: 'relative', overflow: 'hidden', display: 'flex', flexDirection: 'column' }}>
 
       {/* VIDEO BG */}
-      {/**
-       * <video
+      
+        <video
         src={BG_VIDEO_SRC}
         autoPlay muted loop playsInline
         style={{ position: 'absolute', inset: 0, width: '100%', height: '100%', objectFit: 'cover', opacity: 0.90, zIndex: 0 }}
       />
-       * 
-       */}
+        
+       
 
       {/* OVERLAY */}
       <div style={{
